7895838b6140344796d55167f61e7744349474d3
gsingh1017 <182170594+gsingh1017@users.noreply.github.com>
2025-05-22 11:24:14 -0700

    Add Ec2 Stack

--- project/bin/project.ts
+++ project/bin/project.ts
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
+import "source-map-support/register";
 import * as cdk from 'aws-cdk-lib';
 import { ProjectStack } from '../lib/project-stack';
+import { EC2Stack } from '../lib/ec2-stack';
 
 const app = new cdk.App();
 const vpcStack = new ProjectStack(app, 'ProjectStack', {
   
 });
 
+new EC2Stack(app, "MyEC2Stack", {
+  vpc: vpcStack.vpc,
+});
 
 app.synth();
